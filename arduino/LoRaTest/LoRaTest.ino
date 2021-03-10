da8da28f94482c13822e87f26844f2651b70a492
jurriandoornbos <59558756+jurriandoornbos@users.noreply.github.com>
2021-03-10 13:44:28 +0100

    udpates with de lora

--- arduino/LoRaTest/LoRaTest.ino
+++ arduino/LoRaTest/LoRaTest.ino
@@ -2,30 +2,35 @@
 
 SoftwareSerial mySerial(2, 3); //TX, RX
 // (Send and Receive)
-
+String message = "Bericht $";
+int i = 0;
 
 void setup() {
   Serial.begin(9600);
-  while (!Serial){
-  ;
-  }
+  //while (!Serial){
+  //;
+  //}
   mySerial.begin(9600);
 }
 
 void loop() {
+
+  mySerial.println(message + i);
+  delay(1000);
   
-  
-  if(Serial.available() > 0){//Read from serial monitor and send over LoRa wireless module
-    String input = Serial.readStringUntil('$');
-    input.trim();
-    mySerial.print(input);
-    mySerial.println(); 
-  }
+  //if(Serial.available() > 0){//Read from serial monitor and send over LoRa wireless module
+    //String input = Serial.readStringUntil('$');
+    //input.trim();
+    //mySerial.print(input);
+    //mySerial.println(); 
+  //}
  
-  if(mySerial.available() > 1){//Read from LoRa wireless module and send to serial monitor
-    String input = mySerial.readStringUntil('$');
-    input.trim();
-    Serial.print(input);
-    Serial.println();
-  }
+  //if(mySerial.available() > 1){//Read from LoRa wireless module and send to serial monitor
+   // String input = mySerial.readStringUntil('$');
+   // input.trim();
+   // Serial.print(input);
+   // Serial.println();
+  //}
+  i+=1;
+  Serial.println(i);
 }
--- python_testing_serial/lora_receive.py
+++ python_testing_serial/lora_receive.py
@@ -5,7 +5,7 @@ ser = serial.Serial(device, baudrate = 9600,write_timeout=0.1)
 time.sleep(2)
 
 while True:
-    result = ser.readline()
+    result = ser.readline().decode("ascii")
     print(result)
 
 # 64 bytes max, with timeout between each 64 bytes works!
